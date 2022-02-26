9922ca3afc4812f5a215d9f1dad8c8675421b853
Kirthikan <83699363+Kirthikan@users.noreply.github.com>
2022-02-26 09:29:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.position(800,200);
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("my_filter_picture.png");
+}
